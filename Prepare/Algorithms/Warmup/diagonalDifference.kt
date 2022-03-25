3f0a622441e95681bac95601258acd67e03a1a99
Tatsumi0000 <datsunote0211@gmail.com>
2022-03-26 00:15:27 +0900

    Create diagonalDifference.kt

---
+++ Prepare/Algorithms/Warmup/diagonalDifference.kt
@@ -0,0 +1,54 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    var sumPoints = Array<Int>(2, {0})
+    var a = arr.size-1
+    for(i in 0 until arr.size) {
+        sumPoints[0] += arr[i][i]
+        for(j in a downTo 0) {
+            sumPoints[1] += arr[i][j]
+            a -= 1
+            break
+            }
+        }
+    val b = sumPoints[0] - sumPoints[1]
+    return Math.abs(b)
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
