e6d9d849b6367568ea209a27506c5d06768443d4
Tatsumi0000 <datsunote0211@gmail.com>
2022-03-26 00:16:39 +0900

    Create staircase.kt

---
+++ Prepare/Algorithms/Warmup/staircase.kt
@@ -0,0 +1,38 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fun staircase(n: Int): Unit {
+    var sharp = Array(n) { " " }
+    for(i in n-1 downTo 0) {
+        sharp[i] = "#"
+        println(sharp.joinToString(""))
+    }
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    staircase(n)
+}
