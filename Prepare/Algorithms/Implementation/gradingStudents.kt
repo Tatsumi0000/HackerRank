472c9127a60b438f9b853536f8c533b2b704ad0e
Tatsumi0000 <datsunote0211@gmail.com>
2022-03-26 14:51:03 +0900

    いい方法があったからそっちにしたのとコードのフォーマッターを入れた
    importとインデントの整形を自動でしてくれるはず。

--- Prepare/Algorithms/Implementation/gradingStudents.kt
+++ Prepare/Algorithms/Implementation/gradingStudents.kt
@@ -1,25 +1,3 @@
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.round
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
-
 /*
  * Complete the 'gradingStudents' function below.
  *
@@ -36,13 +14,7 @@ fun gradingStudents(grades: Array<Int>): Array<Int> {
             points[i] = grades[i]
             // 点数を5で割ってそれを5で引いたときの数字が3未満だと次の5の倍数に切り上げて良い
         } else if (5 - surplus < 3) {
-            /**
-             * ceilで常に切り上げる
-             * 最も近い5の倍数に切り上げたいので、5で割って商を求める
-             * 出てきた商に+1すると次の5の倍数になるので+1する
-             * 5で割っているからもとに戻すために5をかける
-             */
-            points[i] = ((ceil((grades[i] / 5).toDouble()) + 1) * 5).toInt()
+            points[i] = 5 - surplus + grades[i]
         } else {
             points[i] = grades[i]
         }
