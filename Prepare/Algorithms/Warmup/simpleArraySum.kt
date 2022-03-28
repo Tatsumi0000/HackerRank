c3468509da58ccadfc08092b7074349bd0afbd8c
Tatsumi0000 <datsunote0211@gmail.com>
2022-03-28 23:49:59 +0900

    なんか色々

--- Prepare/Algorithms/Warmup/simpleArraySum.kt
+++ Prepare/Algorithms/Warmup/simpleArraySum.kt
@@ -1,22 +1,3 @@
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
-
 /*
  * Complete the 'simpleArraySum' function below.
  *
@@ -33,7 +14,7 @@ fun simpleArraySum(ar: Array<Int>): Int {
 fun main(args: Array<String>) {
     val arCount = readLine()!!.trim().toInt()
 
-    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val ar = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
 
     val result = simpleArraySum(ar)
 
